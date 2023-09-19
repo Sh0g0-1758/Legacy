6db46e1fc445e703e073df571341a94bb2d9bd0c
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-09-19 15:56:22 +0530

    Completed Shaders

---
+++ OpenGL/ShadersHomework/hwshaders.hpp
@@ -0,0 +1,141 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "../glad/glad.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Shader
+{
+public:
+    unsigned int ID;
+    // constructor generates the shader on the fly
+    // ------------------------------------------------------------------------
+    Shader(const char *vertexPath, const char *fragmentPath)
+    {
+
+        std::string vertexCode;
+        std::string fragmentCode;
+
+        std::ifstream Vertexfile(vertexPath);
+
+        // Check if the file is open
+        if (!Vertexfile.is_open())
+        {
+            std::cerr << "Failed to open the file: " << vertexPath << std::endl;
+        }
+
+        // Read the entire file into a string
+        std::string line1;
+        while (std::getline(Vertexfile, line1))
+        {
+            vertexCode += line1; // Append the line to the string
+            if (!Vertexfile.eof())
+            {
+                vertexCode += '\n'; // Add a newline character for all lines except the last one
+            }
+        }
+
+        // Close the file
+        Vertexfile.close();
+
+        std::ifstream fragmentFile(fragmentPath);
+
+        // Check if the file is open
+        if (!fragmentFile.is_open())
+        {
+            std::cerr << "Failed to open the file: " << fragmentPath << std::endl;
+        }
+
+        // Read the entire file into a string
+        std::string line2;
+        while (std::getline(fragmentFile, line2))
+        {
+            fragmentCode += line2; // Append the line to the string
+            if (!fragmentFile.eof())
+            {
+                fragmentCode += '\n'; // Add a newline character for all lines except the last one
+            }
+        }
+
+        // Close the file
+        fragmentFile.close();
+        const char *vShaderCode = vertexCode.c_str();
+        const char *fShaderCode = fragmentCode.c_str();
+        // 2. compile shaders
+        unsigned int vertex, fragment;
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+        // shader Program
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+        // delete the shaders as they're linked into our program now and no longer necessary
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    // activate the shader
+    // ------------------------------------------------------------------------
+    void use()
+    {
+        glUseProgram(ID);
+    }
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string &name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+    // ------------------------------------------------------------------------
+    void setInt(const std::string &name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    // ------------------------------------------------------------------------
+    void setFloat(const std::string &name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+private:
+    // utility function for checking shader compilation/linking errors.
+    // ------------------------------------------------------------------------
+    void checkCompileErrors(unsigned int shader, std::string type)
+    {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+                          << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                          << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
+};
+#endif
---
+++ OpenGL/ShadersHomework/shader.fs
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 ourPosition;
+void main()
+{
+   FragColor = vec4(ourPosition, 1.0f);
+}
