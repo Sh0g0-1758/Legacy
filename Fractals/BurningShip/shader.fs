07924a7a4cd3561b23465254b8021cd5fe3d0066
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-09-22 01:19:23 +0530

    Initial take at Burning Ship

---
+++ Fractals/BurningShip/shader.fs
@@ -0,0 +1,52 @@
+#version 330 core
+in vec4 gl_FragCoord;
+ 
+out vec4 frag_color;
+ 
+#define MAX_ITERATIONS 600
+ 
+uniform float center_x;
+uniform float center_y;
+uniform float zoom;
+ 
+int get_iterations()
+{
+    float real = ((gl_FragCoord.x / 1080.0f - 0.5f) * zoom + center_x) * 4.0f;
+    float imag = ((gl_FragCoord.y / 1080.0f - 0.7f) * zoom + center_y) * 4.0f;
+
+    int iterations = 0;
+    float const_real = abs(real);
+    float const_imag = abs(imag);
+ 
+    while (iterations < MAX_ITERATIONS)
+    {
+        float tmp_real = abs(real);
+        real = (real * real - imag * imag) + const_real;
+        imag = (2.0f * tmp_real * abs(imag)) + const_imag;
+         
+        float dist = real * real + imag * imag;
+ 
+        if (dist > 4.0f)
+            break;
+ 
+        ++iterations;
+    }
+    return iterations;
+}
+
+vec4 return_color()
+{
+    int iter = get_iterations();
+    if (iter == MAX_ITERATIONS)
+    {
+        return vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+    float iterations = float(iter) / MAX_ITERATIONS;
+    return vec4(iterations,iterations,iterations,1.0f);
+
+}
+ 
+void main()
+{
+    frag_color = return_color();
+}
